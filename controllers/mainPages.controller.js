5a52a27cc2d364b44c4433535f5923537d7546c9
JoOkhafor <jookhafor@gmail.com>
2024-11-29 01:51:39 +0000

    Error handler commented

--- controllers/mainPages.controller.js
+++ controllers/mainPages.controller.js
@@ -3,11 +3,72 @@ function homeGet(req, res) {
   res.render('mainPages/index', { title: "CIGAFI - Nos productions" });
 }
 
+function servicesGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/services', { title: "CIGAFI - Nos productions" });
+}
+
+
+function ressourcesGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/ressources', { title: "CIGAFI - Nos productions" });
+}
+
+function aboutGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/a-propos', { title: "CIGAFI - Nos productions" });
+}
+
+function loginGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/login', { title: "CIGAFI - Nos productions" });
+}
+
+function detailsProprieteGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/details-propriete', { title: "CIGAFI - Nos productions" });
+}
+
+function requeteGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/requete', { title: "CIGAFI - Nos productions" });
+}
+
+function reservationInfoGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/reservation-info', { title: "CIGAFI - Nos productions" });
+}
+
+function reservationPayGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/reservation-pay-mm', { title: "CIGAFI - Nos productions" });
+}
+
+function ressourceDetailsGet(req, res) {
+  console.log(req.body)
+  res.render('mainPages/ressource-details', { title: "CIGAFI - Nos productions" });
+}
+
+
+
+
+
+
+
 
 
 
 
  module.exports = {
-        homeGet
+        homeGet,
+        servicesGet,
+        ressourcesGet,
+        aboutGet,
+        loginGet,
+        detailsProprieteGet,
+        requeteGet,
+        reservationInfoGet,
+        ressourceDetailsGet,
+        reservationPayGet
     };
 
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { decors, getAllRessources, getProductions, detailsRessourcesArticles, detailsRessourcesVideos, entreprise,
-    entreprises, homeGet
+const { homeGet,
+    servicesGet,
+    ressourcesGet,
+    aboutGet,
+    loginGet,
+    detailsProprieteGet,
+    requeteGet,
+    reservationInfoGet,
+    reservationPayGet,
+    ressourceDetailsGet
 } = require('../controllers/mainPages.controller');
 
 /* GET home page. */
 router.get('/', homeGet);
 /* GET home page. */
 router.get('/home', homeGet);
+router.get('/nos-services', servicesGet);
+router.get('/ressources', ressourcesGet);
+router.get('/a-propos', aboutGet);
+router.get('/login', loginGet);
+router.get('/details-propriete', detailsProprieteGet);
+router.get('/requete', requeteGet);
+router.get('/ressource-details', ressourceDetailsGet);
+router.get('/reservation-infos', reservationInfoGet);
+router.get('/reservation-payment', reservationPayGet);
 
 
 module.exports = router;
